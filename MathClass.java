b8e4074679f9352edda031b3842dcf8b1c031ba5
Emily Hines <34042849+MrsHines@users.noreply.github.com>
2019-09-09 13:41:49 -0500

    MathClass Starter File

---
+++ MathClass.java
@@ -0,0 +1,7 @@
+public static class MathClass
+{
+    // ALL of your method headers should follow the pattern: public static <return type> <name>(<parameters>)
+    // This is because we are not writing a constructor for this class.  It is not meant to be replicated as objects but meant to be used
+    // as functions similar to the Math class.
+    
+}
